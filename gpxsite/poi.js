7bf7fa9f68d58fbfc9a8c24fdec8b870e78e2684
felix <felix.rauch@student.uibk.ac.at>
2022-06-21 14:26:48 +0200

    points of interest hinzugefügt

---
+++ gpxsite/poi.js
@@ -0,0 +1,26 @@
+const pointsOfInterest = [{
+    "id": 1,
+    "lat": 47.240945,
+    "lng": 11.417838,
+    "name": "Lanser See",
+    "nype": "Badesee"
+},{
+    "id": 2,
+    "lat": 47.243099,
+    "lng": 11.419246,
+    "name": "Lanser Moor",
+    "type": "Moor"
+}, {
+    "id": 3,
+    "lat": 47.245033,
+    "lng": 11.418901,
+    "name": "Lanser Kopf",
+    "type": "Aussichtspunkt"
+}, {
+    "id": 4,
+    "lat": 47.236820,
+    "lng": 11.417224,
+    "name": "Bushaltestelle Römedihöfe",
+    "type": "Bushaltestelle",
+    "linie": "J"
+}]
