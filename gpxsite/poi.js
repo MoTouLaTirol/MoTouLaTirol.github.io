bc005225a4410b3c70bcdb3c41ce65385e22898a
felix <felix.rauch@student.uibk.ac.at>
2022-06-28 16:43:35 +0200

    Icons in poi.js hinzugefügt

--- gpxsite/poi.js
+++ gpxsite/poi.js
@@ -3,130 +3,151 @@ const pointsOfInterest = [{
     "lat": 47.240945,
     "lng": 11.417838,
     "name": "Lanser See",
-    "type": "Badesee"
+    "type": "Badesee",
+    "icon": "icons/lake.png"
 },{
     "id": 2,
     "lat": 47.243099,
     "lng": 11.419246,
     "name": "Lanser Moor",
-    "type": "Moor"
+    "type": "Moor",
+    "icon": "icons/moor.png"
 }, {
     "id": 3,
     "lat": 47.245033,
     "lng": 11.418901,
     "name": "Lanser Kopf",
-    "type": "Aussichtspunkt"
+    "type": "Aussichtspunkt",
+    "icon": "icons/panoramicview.png"
 }, {
     "id": 4,
     "lat": 47.236820,
     "lng": 11.417224,
     "name": "Bushaltestelle Römedihöfe",
     "type": "Bushaltestelle",
-    "linie": "J"
+    "linie": "J",
+    "icon": "icons/bus.png"
 }, {
     "id": 5,
     "lat": 47.243320,
     "lng": 11.415466,
     "name": "Viller Kopf",
-    "type": "Aussichtspunkt"
+    "type": "Aussichtspunkt",
+    "icon": "icons/panoramicview.png"
 }, {
     "id": 6,
     "lat": 47.242261, 
     "lng": 11.424503,
     "name": "Waldpark Lans",
-    "type": "Spielplatz"
+    "type": "Spielplatz",
+    "icon": "icons/playground.png"
 }, {
     "id": 7,
     "lat": 47.241081, 
     "lng": 11.423800,
     "name": "Tramhaltestelle Lans-Sistrans",
     "type": "Tramhaltestelle",
-    "linie": "6"
+    "linie": "6",
+    "icon": "icons/tramway.png"
 }, {
     "id": 8,
     "lat": 47.236465, 
     "lng": 11.402310,
     "name": "Bushaltestelle Vill Dorfstraße",
     "type": "Bushaltestelle", 
-    "linie": "4141, J, N7, N16"
+    "linie": "4141, J, N7, N16",
+    "icon": "icons/bus.png"
 }, {
     "id": 9,
     "lat": 47.231552,  
     "lng": 11.409949,
     "name": "Kneipp Anlage Igls",
-    "type": "Kneipp Anlage"
+    "type": "Kneipp Anlage",
+    "icon": "icons/lake.png"
 }, {
     "id": 10,
     "lat": 47.231024,  
     "lng": 11.409403,
     "name": "Tourismusinformation Igls",
-    "type": "Information"
+    "type": "Information",
+    "icon": "icons/information.png"
 }, {
     "id": 11,
     "lat": 47.237804,  
     "lng": 11.419365,
     "name": "Parkplatz Lanser See",
-    "type": "Parkplatz"
+    "type": "Parkplatz",
+    "icon": "icons/parking.png"
 }, {
     "id": 12,
     "lat": 47.241097,  
     "lng": 11.416739,
     "name": "Koi Bar Lanser See",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 13,
     "lat": 47.230855,  
     "lng": 11.410898,
     "name": "Landgasthaus Ägidihof",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 14,
     "lat": 47.230282,  
     "lng": 11.411782,
     "name": "Pizzaria Venezia",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 15,
     "lat": 47.238621,  
     "lng": 11.433595,
     "name": "Gasthof Isserwirt",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 16,
     "lat": 47.238436,  
     "lng": 11.432948,
     "name": "Gasthaus zum Wilden Mann",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 17,
     "lat": 47.237950,  
     "lng": 11.428429,
     "name": "Gasthof Walzl",
-    "type": "Restaurant"
+    "type": "Restaurant",
+    "icon": "icons/restaurant.png"
 }, {
     "id": 18,
     "lat": 47.230003,  
     "lng": 11.408243,
     "name": "SPAR",
-    "type": "Lebensmittelladen"
+    "type": "Lebensmittelladen",
+    "icon": "icons/supermarket.png"
 }, {
     "id": 19,
     "lat": 47.240929,  
     "lng": 11.435331,
     "name": "MPREIS",
-    "type": "Lebensmittelladen"
+    "type": "Lebensmittelladen",
+    "icon": "icons/supermarket.png"
 }, {
     "id": 20,
     "lat": 47.234977,  
     "lng": 11.402176,
     "name": "Direkt vom Bauernhof",
-    "type": "Hofladen"
+    "type": "Hofladen",
+    "icon": "icons/farmstand.png"
 }, {
     "id": 21,
     "lat": 47.238513,  
     "lng": 11.433891,
     "name": "Noringerhof Hofladen",
-    "type": "Hofladen"
+    "type": "Hofladen",
+    "icon": "icons/farmstand.png"
 }
 
 ]
